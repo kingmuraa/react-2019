f708d2ee308b4399c336743d0a6fdfbc3008f3d8
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-06-12 22:07:42 +0900

    #10.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ fav }){
+   return <h1>I like {fav}</h1>
+}
 
 function App() {
-   return <div>Hello <Potato /></div>
-   
+   return (
+      <div>
+         <h1>Hello</h1>
+         <Food fav ="kimchi" />
+         <Food fav ="ramen" />
+         <Food fav ="samgiopsal" />
+         <Food fav ="chukumi" />
+
+      </div>
+
+   );
 }
 
 export default App;
