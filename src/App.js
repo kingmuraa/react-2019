97bb1d3abbb765c1383b674541afd23b0a255e2d
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-06-13 22:51:49 +0900

    #10.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React from "react";
 
-function Food({ fav }){
-   return <h1>I like {fav}</h1>
+function Food({ name, picture }){
+   return <div>
+         <h2> I like {name}</h2>
+         <img src={picture} />
+      </div>
 }
+
+const foodILike = [
+   {
+   name: "Kimchi",
+   image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+   },
+   {
+   name: "bulgogi",
+   image: "https://www.hapskorea.com/wp-content/uploads/2011/03/bulgogi.jpg"
+   },
+   {
+   name: "kimbap",
+   image: "https://www.tsunagulocal.com/wp-content/uploads/2021/02/pixta_65991015_M.jpg"
+   },
+   {
+   name: "samgyetang",
+   image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+   }
+];
+   
 
 function App() {
    return (
       <div>
-         <h1>Hello</h1>
-         <Food fav ="kimchi" />
-         <Food fav ="ramen" />
-         <Food fav ="samgiopsal" />
-         <Food fav ="chukumi" />
-
+         {foodILike.map(dish => (
+            <Food name={dish.name} picture={dish.image} /> 
+            ))}
       </div>
-
-   );
+      );
 }
 
 export default App;
